93882d77733b0150a2b6fcd906ebb3cbac0e92c0
Ezequiel Romeo <ezequiel.romeo@gmail.com>
2024-05-30 16:25:49 -0300

    css update

--- js/script.js
+++ js/script.js
@@ -45,58 +45,58 @@ const datosExtra = [
 ];
 
 const datosResumenSegundoPanel = [
-    { texto: 'Estado: Coms. Preinicio 7/9', circle: 'red' },
-    { texto: 'Config.: ok: Fundae, Online', circle: 'green' },
-    { texto: 'Tutores: ko, 2 ediciones sin asignar', circle: 'red' },
-    { texto: 'Proveedor: ok', circle: 'green' },
-    { texto: 'Calendario: ok', circle: 'green' },
-    { texto: 'Alumnos: ko, 30 asignación temp', circle: 'red' }
+    { texto: 'Estado: Coms. Preinicio 7/<p class="text-red">9</p>', circle: 'red' },
+    { texto: 'Config.: <p class="text-green ms-1">ok</p>: Fundae, Online', circle: 'green' },
+    { texto: 'Tutores: <p class="text-red ms-1">ko</p>, 2 ediciones sin asignar', circle: 'red' },
+    { texto: 'Proveedor: <p class="text-green ms-1">ok</p>', circle: 'green' },
+    { texto: 'Calendario: <p class="text-green ms-1">ok</p>', circle: 'green' },
+    { texto: 'Alumnos: <p class="text-red ms-1">ko</p>, 30 asignación temp', circle: 'red' }
 ];
 
 const datosActividadSegundoPanel = [
     { texto: 'Calendario: 9 grupos [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' },
-    { texto: 'Alumnos Ins./Fin: 214/90', circle: 'sin svg' },
-    { texto: 'Cuestionarios: ok, 7 grps. 50%', circle: 'green' },
-    { texto: 'Diplomas: ok, 7 grps. 90%', circle: 'green' }
+    { texto: 'Alumnos Ins./Fin: 214/<p class="text-green">90</p>', circle: 'sin svg' },
+    { texto: 'Cuestionarios: <p class="text-green ms-1">ok</p>, 7 grps. 50%', circle: 'green' },
+    { texto: 'Diplomas: <p class="text-green ms-1">ok</p>, 7 grps. 90%', circle: 'green' }
 ];
 
 const datosExtraSegundoPanel = [
     { texto: 'CIF Centro: ok: B84465863', circle: 'red' },
-    { texto: 'Tutores: ko (<a class="aVer" href="#">desplegar</a>)', circle: 'green' },
-    { texto: 'Expediente: ok: B23222024', circle: 'green' },
+    { texto: 'Tutores: <p class="text-red ms-1 me-1">ko</p> (<a class="aVer" href="#">desplegar</a>)', circle: 'green' },
+    { texto: 'Expediente: <p class="text-green ms-1">ok</p>: B23222024', circle: 'green' },
     { texto: 'Gestión Externa: no', circle: 'sin svg' },
     { texto: 'AV: no', circle: 'sin svg' },
     { texto: 'Online: si', circle: 'sin svg' },
-    { texto: 'Costes: Sin asignar (9 grupos)', circle: 'blue' }
+    { texto: 'Costes: <p class="text-blue ms-1">Sin asignar (9 grupos)</p>', circle: 'blue' }
 ];
 
 const datosGrupo1 = [
     { texto: 'Ini/fin: 1 enero-31 marzo', circle: 'sin svg' },
-    { texto: 'Alumnos/fin: 32/18', circle: 'sin svg' },
+    { texto: 'Alumnos/fin: 32/<p class="text-green">18</p>', circle: 'sin svg' },
     { texto: 'Tutores: 1 DP [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' }
 ];
 
 const datosGrupo2 = [
     { texto: 'Ini/fin: 3 enero-31 marzo', circle: 'sin svg' },
-    { texto: 'Alumnos/fin: 2/1', circle: 'sin svg' },
+    { texto: 'Alumnos/fin: 2/<p class="text-green">1</p>', circle: 'sin svg' },
     { texto: 'Tutores: 1 MG [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' }
 ];
 
 const datosGrupo3 = [
     { texto: 'Ini/fin: 12 enero-31 marzo', circle: 'sin svg' },
-    { texto: 'Alumnos/fin: 22/10', circle: 'sin svg' },
+    { texto: 'Alumnos/fin: 22/<p class="text-green">10</p>', circle: 'sin svg' },
     { texto: 'Tutores: 1 DP [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' }
 ];
 
 const datosGrupo4 = [
     { texto: 'Ini/fin: 3 mayo-31 julio', circle: 'sin svg' },
-    { texto: 'Alumnos/fin: 70/00', circle: 'sin svg' },
+    { texto: 'Alumnos/fin: 70/<p class="text-red">00</p>', circle: 'sin svg' },
     { texto: 'Tutores: 2 DP, MG [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' }
 ];
 
 const datosGrupo5 = [
     { texto: 'Ini/fin: 1 enero-31 marzo', circle: 'sin svg' },
-    { texto: 'Alumnos/fin: 32/18', circle: 'sin svg' },
+    { texto: 'Alumnos/fin: 32/<p class="text-green">18</p>', circle: 'sin svg' },
     { texto: 'Tutores: 1 DP [<a class="aVer" href="#">ver</a>]', circle: 'sin svg' }
 ];
 
@@ -106,6 +106,7 @@ function llenarTabla(idTabla, datos) {
         const fila = document.createElement('tr');
         const celdaTexto = document.createElement('td');
         celdaTexto.innerHTML = dato.texto;
+        celdaTexto.classList.add('d-flex', 'align-items-center', 'justify-content-start', 'celdaTexto');
         fila.appendChild(celdaTexto);
 
         const celdaCirculo = document.createElement('td');
@@ -138,15 +139,12 @@ window.onload = function() {
 
 document.querySelectorAll('.menu-link').forEach(link => {
     link.addEventListener('click', function(event) {
-        // Prevenir el comportamiento por defecto de navegación
         event.preventDefault();
         
-        // Remover la clase 'active' de todos los enlaces
         document.querySelectorAll('.menu-link').forEach(otherLink => {
             otherLink.classList.remove('active');
         });
         
-        // Añadir la clase 'active' al enlace clickeado
         this.classList.add('active');
     });
 });
